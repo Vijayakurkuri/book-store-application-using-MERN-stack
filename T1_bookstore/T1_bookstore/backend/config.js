f4513bc4ca8af75748ed46ee3fa471d6fb8c9eba
Vijaya Lakshmi Kurukuri <vijayalakshmikurukuri@gmail.com>
2024-01-31 14:03:07 +0530

    book store application using MERN stack

---
+++ T1_bookstore/T1_bookstore/backend/config.js
@@ -0,0 +1,9 @@
+// Constants.js
+const PORT = 5555;
+const mongoDBURL = 'mongodb+srv://root:arjuncvinod@book-store.qoibc7b.mongodb.net/book-collection?retryWrites=true&w=majority';
+
+export { PORT, mongoDBURL };
+
+
+// Please create a free database for yourself.
+// This database will be deleted after tutorial
